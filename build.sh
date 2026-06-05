eefef2dc33beb4730982a0e8c27ef0bb93fe65a6
Jose Soto <Jose.Soto@Samkwang.mx>
2026-06-05 10:13:30 -0500

    Automatizar importación de materiales en el build script

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-pip install -r requirements.txt
+echo "Iniciando optimización de entorno para baja memoria RAM..."
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt --no-cache-dir --src ./tmp
+
+echo "Ejecutando tareas de Django..."
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "Poblando Catálogo Maestro de Resinas desde Excel de forma automática..."
+python manage.py import_material
