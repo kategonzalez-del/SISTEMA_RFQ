1dd902f6e93f6b90d4938e7de01e77a525d51c3f
Jose Soto <Jose.Soto@Samkwang.mx>
2026-06-12 10:38:13 -0500

    Cambios en el settings en storage

--- config_rfq/settings.py
+++ config_rfq/settings.py
@@ -171,6 +171,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Habilitar almacenamiento eficiente y compresión con WhiteNoise
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
