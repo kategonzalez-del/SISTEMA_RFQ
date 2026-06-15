850e6ea9f79a7d417288c1114fa76f7d67934552
Jose Soto <Jose.Soto@Samkwang.mx>
2026-06-15 15:24:06 -0500

    Fix: Enrutamiento absoluto nativo mediante etiquetas de plantilla Django

--- RFQ/urls.py
+++ RFQ/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path
 from . import views
-
-#from .views import analisis_dibujo, rfq_detail_view
-from .views import upload_and_process_rfq
-from .views import process_single_file_async
-from .views import finalize_analysis_status
+from .views import upload_and_process_rfq, process_single_file_async, finalize_analysis_status, check_analysis_status
 
 app_name = 'RFQ'
 
 urlpatterns = [
-
     path('rfq/upload/', upload_and_process_rfq, name='upload_rfq'),
     path('rfq/process-file-async/', process_single_file_async, name='process-file-async'),
-    path('analisis/api/rfq/finalize-status/', views.finalize_analysis_status, name='finalize_analysis_status'),
-    path('api/rfq/check-status/', views.check_analysis_status, name='check_analysis_status'),
+    path('analisis/api/rfq/finalize-status/', finalize_analysis_status, name='finalize_analysis_status'),
+    path('api/rfq/check-status/', check_analysis_status, name='check_analysis_status'),
     path('analisis/api/rfq/cancel/', views.cancel_analysis_view, name='cancel_analysis')
 ]
